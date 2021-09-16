03cf0429644e7a7dd561af6bfe2a1ed79d12bcc4
root <jorlefgithub-47d1s1x5c478@outlook.com>
2021-09-16 16:00:13 +0200

    added js fetch api for index

---
+++ front-end/js/fetchIndexAPI.js
@@ -0,0 +1,27 @@
+// fetch api data to show list of products
+fetch("http://localhost:3000/api/teddies")
+    // convert data to json
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        for (let product of data) {
+            // add price 
+            let appendhtml = `
+                <li class="main__listElement">
+                    <a href="./pages/product.html?id=${product._id}">
+                        <img src="${product.imageUrl}" class="main__illustImg">
+                        <div class="p-2 d-flex flex-row justify-content-between align-items-center">
+                            <h2>${product.name}</h2>
+                            <span>${product.price/100}€</span>
+                        </div>
+                    </a>
+                </li>
+            `;
+            document.getElementById("itemsList").innerHTML += appendhtml;
+        }
+    })
+    .catch(function (err) {
+        var errorhtml = '<li class="p-2">Erreur lors de la récupération des informations, rechargez la page</li>';
+        document.getElementById("itemsList").innerHTML += errorhtml;
+    })
