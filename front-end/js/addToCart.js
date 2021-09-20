59a2c6f48de74d25710d2d698e7bcf4bedf021b4
root <jorlefgithub-47d1s1x5c478@outlook.com>
2021-09-20 15:49:51 +0200

    commented

--- front-end/js/addToCart.js
+++ front-end/js/addToCart.js
@@ -1,8 +1,10 @@
+// function to add product to the cart
 function getProductId() {
   let getId = urlGetParams("id");
   let getLocalStorage = localStorage.getItem("products");
   let items = [];
 
+  // conditions to check if localStorage is empty or product already exist in it
   if (getLocalStorage == null) {
     items.push(getId);
     localStorage.setItem("products", items);
